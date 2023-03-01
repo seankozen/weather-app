981f47fc791362ce3c28c0805e1bfb908060ce0a
Sean Kozen <sgkozen@gmail.com>
2023-03-02 00:45:37 +0900

    Initial commit, Buttons and Background

---
+++ src/components/TopButtons.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function TopButtons() {
+  const cities = [
+    { id: 1, title: "Tokyo" },
+    { id: 2, title: "Sydney" },
+    { id: 3, title: "San Francisco" },
+    { id: 4, title: "New York" },
+    { id: 5, title: "London" },
+  ];
+
+  return <div className="flex items-center justify-around my-6">
+    {cities.map((city) => (
+        <button className="text-white text-lg font-medium">{city.title}</button>
+    ))}
+  </div>;
+}
