ee3649465158e7d9e0e3b1e66880b53abbe949a0
Sean Kozen <sgkozen@gmail.com>
2023-03-03 01:18:59 +0900

    Input and Time and Location

---
+++ src/components/TimeAndLocation.js
@@ -0,0 +1,18 @@
+function TimeAndLocation() {
+  return (
+    <div>
+      <div className="flex items-center justify-center my-6">
+        <p className="text-white text-xl font-extralight">
+          Thursday, 2 March 2023 | Local time: 11:30 PM
+        </p>
+      </div>
+      <div className="flex items-center justify-center my-3">
+        <p className="text-white text-3xl font-medium">
+           Tokyo, JP 
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default TimeAndLocation;
--- src/components/TopButtons.js
+++ src/components/TopButtons.js
@@ -11,7 +11,7 @@ export default function TopButtons() {
 
   return <div className="flex items-center justify-around my-6">
     {cities.map((city) => (
-        <button className="text-white text-lg font-medium">{city.title}</button>
+        <button key={city.id} className="text-white text-lg font-medium">{city.title}</button>
     ))}
   </div>;
 }
